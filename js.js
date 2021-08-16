9ec48377eba983da892b3cb7ed2a5406ac85e6aa
shannon1102 <vanictk62@gmail.com>
2021-08-16 15:19:02 +0700

    4k1

---
+++ js.js
@@ -0,0 +1,8 @@
+function yesClick() {
+    alert("Tối nay, lên hồ chơi em nhé!");
+}
+
+function noHover() {
+        var x = Math.floor(Math.random() * (window.innerWidth-200));
+        var y = Math.floor(Math.random() * (window.innerHeight-200));
+}
